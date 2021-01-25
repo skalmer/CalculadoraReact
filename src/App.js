623b0b18936b0fbafca6a6abd972f307f12a36ad
Jonathan Alexander Quezada <jhonq_2@hotmail.com>
2021-01-25 15:30:32 -0500

    calculadoraReact

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Card from "./componentes/card.jsx";
+
+export default function App() {
+  return (
+    <div className="card-container">
+      <Card />
+    </div>
+  );
+}
